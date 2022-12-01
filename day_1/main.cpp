928d2b9659259be99ccb66b1b0ca1cecf756b79b
apre <herculanodavi@gmail.com>
2022-12-01 10:49:13 -0300

    feat: added solutions using generics

--- day_1/main.cpp
+++ day_1/main.cpp
@@ -2,6 +2,28 @@
 #include <iostream>
 #include <queue>
 #include <string>
+#include <type_traits>
+
+template <typename ResultContainer, typename UpdateStrategy,
+          typename ResultStrategy>
+auto for_each_elf(std::ifstream &stream, ResultContainer &&container,
+                  UpdateStrategy &&elf_handler,
+                  ResultStrategy &&result_handler) {
+  std::string line;
+  int total_calories = 0;
+
+  while (!stream.eof()) {
+    getline(stream, line);
+    if (line.empty()) {
+      elf_handler(total_calories, container);
+      total_calories = 0;
+    } else {
+      total_calories += std::stoi(line);
+    }
+  }
+
+  return result_handler(container);
+}
 
 int first_question(std::ifstream &stream) {
   std::string line;
@@ -49,7 +71,7 @@ auto second_question(std::ifstream &stream) {
     current_calories += calories;
   };
 
-  auto reduce = [](decltype(top_three)& priority_queue) {
+  auto reduce = [](decltype(top_three) &priority_queue) {
     decltype(top_three)::value_type sum{};
 
     while (!priority_queue.empty()) {
@@ -93,6 +115,48 @@ int main(int argc, char *argv[]) {
 
   // Second question
   std::cout << "Second question: " << second_question(infile) << "\n";
+
+  // Reset input file stream
+  infile.clear();
+  infile.seekg(0, std::ios::beg);
+
+  // First question as generic code
+  std::cout << "First question -- generic version: "
+            << for_each_elf(
+                   infile, int{0},
+                   [](auto calories, auto &max_container) {
+                     if (calories > max_container) {
+                       max_container = calories;
+                     }
+                   },
+                   [](auto &max_container) -> int { return max_container; })
+            << "\n";
+
+  // Reset input file stream
+  infile.clear();
+  infile.seekg(0, std::ios::beg);
+
+  // Second question as generic code
+  std::cout << "Second question -- generic version: "
+            << for_each_elf(
+                   infile,
+                   std::priority_queue<int, std::vector<int>,
+                                       std::greater<int>>{},
+                   [](int calories, auto &max_container) {
+                     max_container.push(calories);
+                     if (max_container.size() > 3) {
+                       max_container.pop();
+                     }
+                   },
+                   [](auto &max_container) -> int {
+                     int sum{};
+                     while (!max_container.empty()) {
+                       sum += max_container.top();
+                       max_container.pop();
+                     }
+                     return sum;
+                   })
+            << "\n";
 
   return EXIT_SUCCESS;
 }
