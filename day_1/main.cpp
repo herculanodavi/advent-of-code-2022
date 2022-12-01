fd215b60d11cacfe4aec3e90ea3abee9c9395de1
apre <herculanodavi@gmail.com>
2022-12-01 11:12:16 -0300

    feat: replaced original answers with generic answers

--- day_1/main.cpp
+++ day_1/main.cpp
@@ -4,11 +4,9 @@
 #include <string>
 #include <type_traits>
 
-template <typename ResultContainer, typename UpdateStrategy,
-          typename ResultStrategy>
+template <typename ResultContainer, typename UpdateStrategy>
 auto for_each_elf(std::ifstream &stream, ResultContainer &&container,
-                  UpdateStrategy &&elf_handler,
-                  ResultStrategy &&result_handler) {
+                  UpdateStrategy &&elf_handler) {
   std::string line;
   int total_calories = 0;
 
@@ -22,76 +20,7 @@ auto for_each_elf(std::ifstream &stream, ResultContainer &&container,
     }
   }
 
-  return result_handler(container);
-}
-
-int first_question(std::ifstream &stream) {
-  std::string line;
-  int current_calories = 0;
-  int max_calories = 0;
-
-  auto handle_empty = [&current_calories, &max_calories] {
-    if (current_calories > max_calories) {
-      max_calories = current_calories;
-    }
-    current_calories = 0;
-  };
-
-  auto handle_food = [&current_calories](int calories) {
-    current_calories += calories;
-  };
-
-  while (!stream.eof()) {
-    getline(stream, line);
-
-    if (line.empty()) {
-      handle_empty();
-    } else {
-      handle_food(std::stoi(line));
-    }
-  }
-
-  return max_calories;
-}
-
-auto second_question(std::ifstream &stream) {
-  std::string line;
-  int current_calories = 0;
-  std::priority_queue<int, std::vector<int>, std::greater<int>> top_three;
-
-  auto handle_empty = [&current_calories, &top_three] {
-    top_three.push(current_calories);
-    if (top_three.size() > 3) {
-      top_three.pop();
-    }
-    current_calories = 0;
-  };
-
-  auto handle_food = [&current_calories](int calories) {
-    current_calories += calories;
-  };
-
-  auto reduce = [](decltype(top_three) &priority_queue) {
-    decltype(top_three)::value_type sum{};
-
-    while (!priority_queue.empty()) {
-      sum += priority_queue.top();
-      priority_queue.pop();
-    }
-    return sum;
-  };
-
-  while (!stream.eof()) {
-    getline(stream, line);
-
-    if (line.empty()) {
-      handle_empty();
-    } else {
-      handle_food(std::stoi(line));
-    }
-  }
-
-  return reduce(top_three);
+  return container;
 }
 
 int main(int argc, char *argv[]) {
@@ -106,57 +35,41 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  // First question
-  std::cout << "First question: " << first_question(infile) << "\n";
-
-  // Reset input file stream
-  infile.clear();
-  infile.seekg(0, std::ios::beg);
-
-  // Second question
-  std::cout << "Second question: " << second_question(infile) << "\n";
-
-  // Reset input file stream
-  infile.clear();
-  infile.seekg(0, std::ios::beg);
-
   // First question as generic code
-  std::cout << "First question -- generic version: "
-            << for_each_elf(
-                   infile, int{0},
-                   [](auto calories, auto &max_container) {
-                     if (calories > max_container) {
-                       max_container = calories;
-                     }
-                   },
-                   [](auto &max_container) -> int { return max_container; })
-            << "\n";
+  std::cout << "First question: " << [&infile]() {
+    return for_each_elf(infile, int{},
+                        [](int calories, auto &container) -> void {
+                          if (calories > container) {
+                            container = calories;
+                          }
+                        });
+  }() << "\n";
 
   // Reset input file stream
   infile.clear();
   infile.seekg(0, std::ios::beg);
 
   // Second question as generic code
-  std::cout << "Second question -- generic version: "
-            << for_each_elf(
-                   infile,
-                   std::priority_queue<int, std::vector<int>,
-                                       std::greater<int>>{},
-                   [](int calories, auto &max_container) {
-                     max_container.push(calories);
-                     if (max_container.size() > 3) {
-                       max_container.pop();
-                     }
-                   },
-                   [](auto &max_container) -> int {
-                     int sum{};
-                     while (!max_container.empty()) {
-                       sum += max_container.top();
-                       max_container.pop();
-                     }
-                     return sum;
-                   })
-            << "\n";
+  std::cout << "Second question: " << [&infile]() {
+    using ValueType = int;
+    ValueType sum{};
+    auto queue =
+        for_each_elf(infile,
+                     std::priority_queue<ValueType, std::vector<ValueType>,
+                                         std::greater<ValueType>>{},
+                     [](int calories, auto &container) -> void {
+                       container.push(calories);
+                       if (container.size() > 3) {
+                         container.pop();
+                       }
+                     });
+
+    while (!queue.empty()) {
+      sum += queue.top();
+      queue.pop();
+    }
+    return sum;
+  }() << "\n";
 
   return EXIT_SUCCESS;
 }
