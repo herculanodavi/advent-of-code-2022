aaa2efb5ddd109f884d64b7190f16dc3b86ad20f
apre <herculanodavi@gmail.com>
2022-12-06 12:13:29 -0300

    refactor: file is read just once and functions were simplified.

--- day_6/main.cpp
+++ day_6/main.cpp
@@ -1,52 +1,44 @@
 #include <algorithm>
-#include <charconv>
 #include <fstream>
 #include <iostream>
 #include <numeric>
 #include <unordered_set>
-#include <vector>
 
-auto handle_string(std::ifstream &stream, auto &&handler) {
-  std::string line;
-  getline(stream, line);
-  auto result = handler(line);
-  stream.clear();
-  stream.seekg(0, std::ios::beg);
-  return result;
-}
+/*
+ * Returns the number of characters processed until a substring
+ * with <n> different characters is found. Returns -1 if no such substring
+ * is found.
+ */
+auto characters_to_n_different(const std::string &line, int n) -> int {
+  auto p = line.begin();
+  auto q = line.begin();
+  std::unordered_set<char> set{line[0]};
 
-auto characters_to_n_different(std::ifstream &stream, int n) {
-  return handle_string(stream, [&](std::string &line) -> int {
-    auto p = line.begin();
-    auto q = line.begin();
-    std::unordered_set<char> set{line[0]};
-
-    while (q != line.end()) {
-      std::advance(q, 1);
-      if (set.contains(*q)) {
-        while (*p != *q) {
-          set.erase(*p);
-          std::advance(p, 1);
-        }
+  while (q != line.end()) {
+    std::advance(q, 1);
+    if (set.contains(*q)) {
+      while (*p != *q) {
+        set.erase(*p);
         std::advance(p, 1);
-      } else {
-        set.insert(*q);
+      }
+      std::advance(p, 1);
+    } else {
+      set.insert(*q);
 
-        if (set.size() == n) {
-          return std::distance(line.begin(), q) + 1;
-        }
+      if (set.size() == n) {
+        return std::distance(line.begin(), q) + 1;
       }
     }
-    return -1;
-  });
+  }
+  return -1;
 }
 
-auto first_question(std::ifstream &stream) {
-  return characters_to_n_different(stream, 4);
+auto first_question(const std::string &line) -> int {
+  return characters_to_n_different(line, 4);
 }
 
-auto second_question(std::ifstream &stream) {
-  return characters_to_n_different(stream, 14);
+auto second_question(const std::string &line) -> int {
+  return characters_to_n_different(line, 14);
 }
 
 int main(int argc, char *argv[]) {
@@ -61,8 +53,11 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  std::cout << "First question: " << first_question(infile) << "\n";
-  std::cout << "Second question: " << second_question(infile) << "\n";
+  std::string line;
+  getline(infile, line);
+
+  std::cout << "First question: " << first_question(line) << "\n";
+  std::cout << "Second question: " << second_question(line) << "\n";
 
   return EXIT_SUCCESS;
 }
